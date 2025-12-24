c61c6bbcf46bf35d7ce8849ecde9b8edf82824f8
MC-Error <mcerror310@gmail.com>
2025-12-24 11:41:35 +0530

    Add files via upload

--- pair.js
+++ pair.js
@@ -65,9 +65,8 @@ router.get('/', async (req, res) => {
 
                         const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
 
-                        const string_session = mega_url.replace('https://mega.nz/file/', ''
-                                                                
-                        );
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+
                         const sid = string_session;
 
                         const dt = await PrabathPairWeb.sendMessage(user_jid, {
