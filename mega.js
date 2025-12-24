d289c677b192f5a412e0e69b9b0eb354ce56ff0f
MC-Error <mcerror310@gmail.com>
2025-12-24 15:11:21 +0530

    Add files via upload

--- mega.js
+++ mega.js
@@ -1,44 +1,27 @@
 const mega = require("megajs");
 const auth = {
-  email: "mcerror310@gmail.com",
-  password: "mc error123",
-  userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
-};
+    email: 'mcerror310@gmail.com',
+    password: 'mc error123',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+}
 
 const upload = (data, name) => {
-  return new Promise((resolve, reject) => {
-    const storage = new mega.Storage(auth);
-
-    // Wait for storage to be ready
-    storage.on("ready", () => {
-      console.log("Storage is ready. Proceeding with upload.");
-
-      const uploadStream = storage.upload({ name, allowUploadBuffering: true });
-
-      uploadStream.on("complete", (file) => {
-        file.link((err, url) => {
-          if (err) {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) throw err;
+                        storage.close()
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
             reject(err);
-          } else {
-            storage.close();
-            resolve(url);
-          }
-        });
-      });
-
-      uploadStream.on("error", (err) => {
-        reject(err);
-      });
-
-      data.pipe(uploadStream);
+        }
     });
-
-    storage.on("error", (err) => {
-      reject(err);
-    });
-  });
 };
 
 module.exports = { upload };
-
